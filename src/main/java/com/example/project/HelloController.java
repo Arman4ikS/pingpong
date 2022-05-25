6ff45751d560c1d8f149e0e0b6cf5e8a66150a67
Anjleil <timofandr@gmail.com>
2022-05-25 16:10:33 +0300

    ХЗ что коммичу

--- src/main/java/com/example/project/HelloController.java
+++ src/main/java/com/example/project/HelloController.java
@@ -1,57 +1,133 @@
 package com.example.project;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.fxml.FXML;
-import javafx.scene.Cursor;
+import java.util.Random;
+import javafx.animation.KeyFrame;
+import javafx.animation.Timeline;
+import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.shape.Circle;
-import javafx.scene.input.KeyEvent;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.TextAlignment;
+import javafx.stage.Stage;
+import javafx.util.Duration;
 
+public class HelloController extends Application {
 
-public class HelloController {
-    @FXML
-    Button leftButton, rightButton;
-    @FXML
-    Button startButton;
-    @FXML
-    Circle circle;
-    double lbutton;
-    double rbutton;
+    //variable
+    private static final int width = 1000;
+    private static final int height = 600;
+    private static final int PLAYER_HEIGHT = 100;
+    private static final int PLAYER_WIDTH = 15;
+    private static final double BALL_R = 15;
+    private int ballYSpeed = 3;
+    private int ballXSpeed = 3;
+    private double playerOneYPos = height / 2;
+    private double YPos = height / 2;
+    private double playerTwoYPos = height / 2;
+    private double ballXPos = width / 2;
+    private double ballYPos = height / 2;
+    private int scoreP1 = 0;
+    private int scoreP2 = 0;
+    private boolean gameStarted;
+    private int playerOneXPos = 10;
+    private double playerTwoXPos = width - PLAYER_WIDTH - playerOneXPos;
 
-    boolean player1Up = false;
-    boolean player1Down = false;
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("P O N G");
+        //background size
+        Canvas canvas = new Canvas(width, height);
+        GraphicsContext gc = canvas.getGraphicsContext2D();
 
+        //JavaFX Timeline = free form animation defined by KeyFrames and their duration
+        Timeline tl = new Timeline(new KeyFrame(Duration.millis(10), e -> run(gc)));
+        //number of cycles in animation INDEFINITE = repeat indefinitely
+        tl.setCycleCount(Timeline.INDEFINITE);
 
-    public void start(MouseEvent mouseEvent) {
-        startButton.setVisible(false);
-        Scene scene = leftButton.getScene();
-        scene.setOnKeyTyped(new EventHandler<KeyEvent>() {
-            public void handle(KeyEvent ke) {
-                if ((ke.getCharacter().equals("w"))&&(leftButton.getLayoutY()>=0)){
-                    leftButton.setLayoutY(leftButton.getLayoutY()-1);
-                } else if ((ke.getCharacter().equals("s"))&&(leftButton.getLayoutY()<=HelloApplication.height)){
-                    leftButton.setLayoutY(leftButton.getLayoutY()+1);
-                }
-            }
-        });
+        //mouse control (move and click)
+        canvas.setOnMouseMoved(e ->  playerOneYPos = e.getY() - PLAYER_HEIGHT/2);
+        canvas.setOnMouseClicked(e ->  gameStarted = true);
+        stage.setScene(new Scene(new StackPane(canvas)));
+        stage.show();
+        tl.play();
     }
 
-    public void left1() {
+    private void run(GraphicsContext gc) {
+        //set graphics
+        //set background color
+        gc.setFill(Color.BLACK);
+        gc.fillRect(0, 0, width, height);
+
+        //set text
+        gc.setFill(Color.WHITE);
+        gc.setFont(Font.font(30));
+
+        if(gameStarted) {
+
+            //set ball movement
+            ballXPos+=ballXSpeed;
+            ballYPos+=ballYSpeed;
+
+            if ((ballYPos > 0)&&(ballYPos < height-PLAYER_HEIGHT)){
+                if (ballYPos >= playerTwoYPos) playerTwoYPos+=5;
+                if (ballYPos < playerTwoYPos) playerTwoYPos-=5;
+            }
+
+            //draw the ball
+            gc.fillOval(ballXPos, ballYPos, BALL_R, BALL_R);
+
+        } else {
+            //set the start text
+            gc.setStroke(Color.WHITE);
+            gc.setTextAlign(TextAlignment.CENTER);
+            gc.strokeText("Click", width / 2, height / 2);
+
+            //reset the ball start position
+            ballXPos = width / 2;
+            ballYPos = height / 2;
+
+            //reset the ball speed and the direction
+            ballXSpeed = new Random().nextInt(2) == 0 ? 3: -3;
+            ballYSpeed = new Random().nextInt(2) == 0 ? 3: -3;
+        }
+
+        //makes sure the ball stays in the canvas
+        if(ballYPos+BALL_R+20 > height || ballYPos < 25) ballYSpeed *=-1;
+
+        //if you miss the ball, computer gets a point
+        if(ballXPos < playerOneXPos - PLAYER_WIDTH) {
+            scoreP2++;
+            gameStarted = false;
+        }
+
+        //if the computer misses the ball, you get a point
+        if(ballXPos > playerTwoXPos + PLAYER_WIDTH) {
+            scoreP1++;
+            gameStarted = false;
+        }
+
+        //increase the speed after the ball hits the player
+        if( ((ballXPos + BALL_R > playerTwoXPos) && ballYPos >= playerTwoYPos && ballYPos <= playerTwoYPos + PLAYER_HEIGHT) ||
+                ((ballXPos < playerOneXPos + PLAYER_WIDTH) && ballYPos >= playerOneYPos && ballYPos <= playerOneYPos + PLAYER_HEIGHT)) {
+            ballYSpeed += 1 * Math.signum(ballYSpeed);
+            ballXSpeed += 1 * Math.signum(ballXSpeed);
+            ballXSpeed *= -1;
+            ballYSpeed *= 1;
+        }
+
+        //draw score
+        gc.fillText(scoreP1 + "\t\t\t\t\t\t\t\t" + scoreP2, width / 2, 100);
+        //draw player 1 & 2
+        gc.fillRect(playerTwoXPos, playerTwoYPos, PLAYER_WIDTH, PLAYER_HEIGHT);
+        gc.fillRect(playerOneXPos, playerOneYPos, PLAYER_WIDTH, PLAYER_HEIGHT);
+        gc.fillRect(35, 10, 930, 10);
+        gc.fillRect(35, 580, 930, 10);
     }
 
-    public void right1(MouseEvent mouseEvent) {
-        rbutton = mouseEvent.getY();
-        rightButton.setOnMouseDragged(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent mouseEvent) {
-                rightButton.setLayoutY(mouseEvent.getSceneY() - rbutton);
-
-            }
-        });
+    // start the application
+    public static void main(String[] args) {
+        launch(args);
     }
 }
-
--- src/main/java/com/example/project/MenuController.java
+++ src/main/java/com/example/project/MenuController.java
@@ -10,18 +10,21 @@ import javafx.scene.control.Button;
 
 public class MenuController {
     @FXML
-    Button startButton1P;
+    public Button startButton1P;
     @FXML
     Button startButton2P;
 
     public void start1P() throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("OnePlayer.fxml"));
-        Stage window = (Stage) startButton1P.getScene().getWindow();
-        window.setScene(new Scene(root, HelloApplication.width, HelloApplication.height));
+        //Parent root = FXMLLoader.load(getClass().getResource("OnePlayer.fxml"));
+        //Stage window = (Stage) startButton1P.getScene().getWindow();
+        //window.setScene(new Scene(root, HelloApplication.width, HelloApplication.height));
+
     }
     public void start2P() throws Exception{
 
     }
+
+
 
 }
 
